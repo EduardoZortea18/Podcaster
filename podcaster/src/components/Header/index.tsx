dba043b8078f6c5373463acc20b24f7bec4bffb8
EduardoZortea18 <60518656+EduardoZortea18@users.noreply.github.com>
2021-04-21 11:22:03 -0300

    criacao do header e player da pagina

---
+++ podcaster/src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import format from 'date-fns/format';
+import enUS from 'date-fns/locale/en-US';
+
+import styles from './styles.module.scss';
+
+export function Header(){
+
+  const currentDate = format(new Date(), 'E, MMMM do', {locale: enUS});
+
+  return(
+    <header className={styles.headerContainer}>
+      <img src="/logo.svg" alt="Podcaster"/>
+
+      <p>The for you to listen all the time</p>
+      <span>{currentDate}</span>
+    </header>
+  );
+}
---
+++ podcaster/src/components/Player/index.tsx
@@ -0,0 +1,48 @@
+import styles from './styles.module.scss';
+
+export function Player(){
+  return(
+   <div className={styles.playerContainer}>
+     <header>
+       <img src="/playing.svg" alt="playing now"/>
+       <strong>Playing now</strong>
+     </header>
+
+     <div className={styles.emptyPlayer}>
+        <strong>Choose a podcast</strong>
+     </div>
+
+     <footer className={styles.empty}>
+       <div className={styles.progress}>
+          <span>00:00</span>
+          <div className={styles.slider}>
+            <div className={styles.emptySlider}/>
+          </div>
+          <span>00:00</span>
+       </div>
+
+       <div className={styles.buttons}>
+         <button type="button">
+            <img src="shuffle.svg" alt="shuffle"/>
+         </button>
+
+         <button type="button">
+            <img src="play-previous.svg" alt="play previous"/>
+         </button>
+
+         <button type="button" className={styles.playButton}>
+            <img src="play.svg" alt="play"/>
+         </button>
+
+         <button type="button">
+            <img src="play-next.svg" alt="play next"/>
+         </button>
+
+         <button type="button">
+            <img src="repeat.svg" alt="repeat"/>
+         </button>
+       </div>
+     </footer>
+   </div>
+  );
+}
